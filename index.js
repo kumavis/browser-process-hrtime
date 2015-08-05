8d75f29db66f9c8e9b9d894585dfbd83f8bdb7cd
kumavis <aaron@kumavis.me>
2015-08-04 17:19:41 -0700

    fix e-notation

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ var performanceNow =
 // generate timestamp or delta
 // see http://nodejs.org/api/process.html#process_process_hrtime
 function hrtime(previousTimestamp){
-  var clocktime = performanceNow.call(performance)/10e3
+  var clocktime = performanceNow.call(performance)*1e-3
   var seconds = Math.floor(clocktime)
-  var nanoseconds = (clocktime%1)*10e9
+  var nanoseconds = Math.floor((clocktime%1)*1e9)
   if (previousTimestamp) {
     seconds = seconds - previousTimestamp[0]
     nanoseconds = nanoseconds - previousTimestamp[1]
     if (nanoseconds<0) {
       seconds--
-      nanoseconds += 10e9
+      nanoseconds += 1e9
     }
   }
   return [seconds,nanoseconds]
