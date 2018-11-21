b7b3588595790c09d843ef84d252c74d19806ea3
Ira Miller <public@iramiller.com>
2018-11-21 14:23:34 -0500

    UMD with html example

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-module.exports = process.hrtime || hrtime
-
-// polyfil for window.performance.now
-var performance = global.performance || {}
-var performanceNow =
-  performance.now        ||
-  performance.mozNow     ||
-  performance.msNow      ||
-  performance.oNow       ||
-  performance.webkitNow  ||
-  function(){ return (new Date()).getTime() }
-
-// generate timestamp or delta
-// see http://nodejs.org/api/process.html#process_process_hrtime
-function hrtime(previousTimestamp){
-  var clocktime = performanceNow.call(performance)*1e-3
-  var seconds = Math.floor(clocktime)
-  var nanoseconds = Math.floor((clocktime%1)*1e9)
-  if (previousTimestamp) {
-    seconds = seconds - previousTimestamp[0]
-    nanoseconds = nanoseconds - previousTimestamp[1]
-    if (nanoseconds<0) {
-      seconds--
-      nanoseconds += 1e9
-    }
+;(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = root.process.hrtime || factory()
+  } else {
+    // Browser globals (root is window)
+    // include process polyfil
+    root.process = root.process || {}
+    root.process.hrtime = root.process.hrtime || factory();
   }
-  return [seconds,nanoseconds]
-}
+}((typeof global === 'object' && global.global === global && global) ||
+    (typeof window === 'object' && window.window === window && window) ||
+    (typeof self === 'object' && self.self === self && self) ||
+    this, function () {
+  // polyfil for window.performance.now
+  var performance = performance || {}
+  var performanceNow =
+    performance.now        ||
+    performance.mozNow     ||
+    performance.msNow      ||
+    performance.oNow       ||
+    performance.webkitNow  ||
+    function(){ return (new Date()).getTime() }
+
+  // generate timestamp or delta
+  // see http://nodejs.org/api/process.html#process_process_hrtime
+  return function hrtime (previousTimestamp) {
+    var clocktime = performanceNow.call(performance)*1e-3
+    var seconds = Math.floor(clocktime)
+    var nanoseconds = Math.floor((clocktime%1)*1e9)
+    if (previousTimestamp) {
+      seconds = seconds - previousTimestamp[0]
+      nanoseconds = nanoseconds - previousTimestamp[1]
+      if (nanoseconds<0) {
+        seconds--
+        nanoseconds += 1e9
+      }
+    }
+    return [seconds,nanoseconds]
+  }
+}));
